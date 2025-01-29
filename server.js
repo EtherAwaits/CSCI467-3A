09f93d6d4d23895f068f70cfa2c24b9dccb6d31f
EtherAwaits <brettwilliams343@gmail.com>
2025-01-29 17:51:30 -0600

    ⛵tailwind support

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running on localhost:"+ PORT)
+        console.log(`Server is Successfully Running on localhost:${PORT}`)
     else 
         console.log("Error occurred, server can't start", error);
     }
