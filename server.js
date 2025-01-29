b062d5603c7d07394b1bdcd75f87b990c56deaf9
EtherAwaits <brettwilliams343@gmail.com>
2025-01-29 16:38:38 -0600

    🖥️base framework setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('src'));
+const PORT = 3000;
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running on localhost:"+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
