6b29c8966a54fc1bb5b976614b107f52c3945205
Ryan Groch <ryan.w.groch@gmail.com>
2025-04-02 14:19:32 -0500

    Adds legacy db connection

--- db.js
+++ db.js
@@ -14,9 +14,18 @@ if (dotenv.error) {
 
 const DB_URL = dotenv.parsed.DB_URL;
 
-// The actual database connection
+// Our database connection
 const con = mysql.createConnection(DB_URL);
+
+// legacy db connection
+const legacy_con = mysql.createConnection({
+  host: "blitz.cs.niu.edu",
+  user: "student",
+  password: "student",
+  database: "csci467",
+});
 
 module.exports = {
   con,
+  legacy_con,
 };
